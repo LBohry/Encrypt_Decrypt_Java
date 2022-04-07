a81ce8f40b65caae42df2b451ba74964a0058dec
LBohry <83679014+LBohry@users.noreply.github.com>
2022-04-08 00:26:47 +0100

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main implements ActionListener {
+    JFrame frame;
+    JTextField textField;
+    JButton encrypt;
+    JButton decrypt;
+
+    JPanel panel;
+
+    Main(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700,420);
+
+        textField = new JTextField();
+
+        encrypt = new JButton("Encrypt");
+        decrypt = new JButton("Decrypt");
+
+        encrypt.addActionListener(this);
+        decrypt.addActionListener(this);
+    }
+    public static void main(String[] args) {
+        Main e = new Main();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
